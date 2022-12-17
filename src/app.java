f282e268458a794fbb036a6f5ebec04fd14879b9
Gustavo Correia <gucorreia2901@gmail.com>
2022-12-17 01:59:10 -0300

    Testando o adicionamento de commits pelo bash e apredendo a usar o git

---
+++ src/app.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class app {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("ko");
+        System.out.println("num:");
+        int num1 = sc.nextInt();
+
+        System.out.println(num1);
+
+        sc.close();
+    }
+}
