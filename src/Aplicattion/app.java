2c9016bbe742a93e827c6847e9559009114a1d86
Gustavo Correia <gucorreia2901@gmail.com>
2022-12-17 19:49:40 -0300

    Adicionado Função de Sobrecarga com o objetivo de contas as vogais de uma String

--- src/Aplicattion/app.java
+++ src/Aplicattion/app.java
@@ -8,18 +8,27 @@ public class app {
         
         Scanner sc = new Scanner(System.in);
         mat extensao = new mat();
+        int num1[] = new int[11];
         
-        System.out.println("num:");
-        int num1 = sc.nextInt();
+        System.out.println("primeiro número:");
+        num1[0] = sc.nextInt();
+        System.out.println("Seu numero é > " + num1[0]);
+        System.out.println("Número em ordem crescente abaixo:");
+        for (int x=0; x<11; x++){
+            num1[x] = num1[0] + x;
+            System.out.println(num1[x]);
+        }
         System.out.println("Digite seu nome:");
-        extensao.totalletras = sc.next();
-        System.out.println("Seu nome é > " + extensao.totalletras);
-        System.out.println("Seu numero é > " + num1);
+        String totalletras = sc.next();
+        System.out.println("Seu nome é > " + totalletras);
         
-        int letras = extensao.Calcletras();
+        
+        int letras = extensao.Calcletras(totalletras);
 
         System.out.println("O seu nome tem: " + letras + " letras ao total");
 
+        int vogais = extensao.Calcletras(totalletras,letras);
+        System.out.println("total de vogais no seu nome: " + vogais);
         sc.close();
     }
 }
--- src/Extensao/mat.java
+++ src/Extensao/mat.java
@@ -2,14 +2,41 @@ package Extensao;
 
 
 public class mat {
-    public String totalletras;
+    
 
 
 
-    public int Calcletras(){
+    public int Calcletras(String totalletras){
         return totalletras.length();
     }
+    public int Calcletras(String totalString, int letras){
+        char temp;
+        int totaldevogais=0;
+        for (int x=0; x<letras; x++){
+            temp = totalString.charAt(x);
+            if (temp == 'a'){
+                totaldevogais++;
+            }
+            
+            if (temp == 'e'){
+                totaldevogais++;
+            }
+            
+            if (temp == 'i'){
+                totaldevogais++;
+            }
+            
+            if (temp == 'o'){
+                totaldevogais++;
+            }
 
+            if (temp == 'u'){
+                totaldevogais++;
+            }
+
+        }
+        return totaldevogais;
+    }
 
 
 
